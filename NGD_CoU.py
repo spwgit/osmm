c719db43c1e8645a570b39c365385730931e8e35
spwgit <simonp.warren@gmail.com>
2023-02-03 21:06:01 +0000

    Add files via upload

--- NGD_CoU.py
+++ NGD_CoU.py
@@ -1,8 +1,29 @@
+from urllib.request import urlopen
+import pprint
+import requests
 from osdatahub import DataPackageDownload
+import json
 
 key = 'gP228DpHXZ2BWdrmffMmUNhzAFyuuE27'
-dpdl = DataPackageDownload.all_products(key)
-#print(type(dpdl))
-for i in dpdl:
-    if i["id"] == "929":
-        print(i["url"])
+data = DataPackageDownload.all_products(key)
+#print(type(data))
+#pprint.pprint(data)
+
+for item in data:
+    if type(item) is dict:
+        if item["id"] == "929" and item["versions"][0]["id"] == "2361":
+            dlUrl = item["versions"][0]["url"]
+            #print(item)
+            print("-------------------------------------------------------------------------")
+            print(dlUrl)
+
+webURL = urlopen(dlUrl)
+dl = webURL.read()
+encoding = webURL.info().get_content_charset('utf-8')
+JSON_object = json.loads(dl.decode(encoding))
+
+for u in JSON_object:
+    if u == "downloads":
+        for i in u:
+            pprint.pprint(i)
+    print("-----------------------")

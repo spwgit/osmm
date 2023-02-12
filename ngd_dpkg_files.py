3e75826237d37789b4be4f95ef11860e656827e0
spwgit <simonp.warren@gmail.com>
2023-02-12 21:10:01 +0000

    Update ngd_dpkg_files.py

--- ngd_dpkg_files.py
+++ ngd_dpkg_files.py
@@ -3,7 +3,20 @@ from urllib.request import urlopen
 import gdaltools
 import json
 import requests
-import os, zipfile
+import os, platform, zipfile
+
+print(platform.system()) 
+if platform.system() == 'Linux':
+    dlCachePath = '/home/simon/data/ngd/features/'
+    gpkgPath = '/home/simon/data/ngd/features/'
+    unzipped = 'unzipped/'
+    pgConnStr = 'host="localhost", port=5432, dbname="ngd", schema="features", user="postgres", password="postgres.."'
+else:
+    dlCachePath = 'c:/os_ngd_features/'
+    gpkgPath = "c:\\os_ngd_features\\"
+    unzipped = "unzipped\\"
+    pgConnStr = 'host="sw2-gis.wychavon.gov.uk", port=5432, dbname="ngd", schema="features", user="postgres", password="postgres.."'
+
 
 key = 'gP228DpHXZ2BWdrmffMmUNhzAFyuuE27'
 dp = "929"
@@ -14,13 +27,12 @@ for item in data:
     if type(item) is dict:
         if item["id"] == dp and item["versions"][0]["id"] == v:
             dlUrl = item["versions"][0]["url"]
-            print("dp:", dp, "/v" ,v)
+            print("dp:", dp, "v" ,v)
 
 filesURL = urlopen(dlUrl)
 dl = filesURL.read()
 encoding = filesURL.info().get_content_charset('utf-8')
 files = json.loads(dl.decode(encoding))
-dlCachePath = 'c:/os_ngd_features/'
 
 for f in files['downloads']:
     r = requests.get(f["url"], allow_redirects=True)
@@ -28,23 +40,22 @@ for f in files['downloads']:
     print("...", f["url"][48:])
 
 for item in os.listdir(dlCachePath):
-   with zipfile.ZipFile(dlCachePath + item, 'r') as zObject:
-        zObject.extractall(path= dlCachePath + "/unzipped/")
+    if item != 'unzipped':
+        with zipfile.ZipFile(os.path.join(dlCachePath, item), 'r') as zObject:
+            zObject.extractall(path= dlCachePath + unzipped)
 
-#gdaltools.Wrapper.BASEPATH = "C:\Program Files\GDAL"
 
 ogr = gdaltools.ogr2ogr()
 ogr.set_encoding("UTF-8")
-conn = gdaltools.PgConnectionString(host="sw2-gis.wychavon.gov.uk", port=5432, dbname="ngd", schema="features", user="postgres", password="postgres..")
+conn = gdaltools.PgConnectionString(host="localhost", port=5432, dbname="ngd", schema="features", user="postgres", password="postgres..")
 srs = 'EPSG:27700'
-gpkgPath = "c:\\os_ngd_features\\"
 
-for gpkg in os.listdir(gpkgPath + "unzipped\\"):
-    gpkgname = gpkgPath + "unzipped\\" + gpkg
-    tablename =  table_name=gpkg[:-5]
+for gpkg in os.listdir(os.path.join(gpkgPath, unzipped)):
+    gpkgname = gpkgPath + unzipped + gpkg
+    tablename = gpkg[:-5]
     ogr.set_input(gpkgname, srs=srs)
     print(gpkgname, srs)
     ogr.set_output(conn, table_name=tablename, srs=srs)
     print(conn, tablename, srs)    
     ogr.execute()
-    
+
